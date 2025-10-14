26896651c49da611c2038b9bfbe8490b02d0b00b
vvsluv <uuu100500@mail.ru>
2025-10-14 13:17:24 +0300

    обновление игры

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1760367892|12773110404';
+const CACHE_VERSION = '1760436977|14635358586';
 /** @type {string} */
 const CACHE_PREFIX = 'Лудик-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
