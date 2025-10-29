831717f50e9fa31278a4837a86cc46f2630f2600
vvsluv <uuu100500@mail.ru>
2025-10-29 11:51:09 +0300

    Улучшение производительности и новые механики

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761503961|21557014833';
+const CACHE_VERSION = '1761727821|9275631788';
 /** @type {string} */
 const CACHE_PREFIX = 'лудик-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
