731b089f54faef36880133583f1ca0ddf972a851
vvsluv <uuu100500@mail.ru>
2025-10-29 15:10:00 +0300

    фикс багов

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761739194|4524071271';
+const CACHE_VERSION = '1761739774|5104586068';
 /** @type {string} */
 const CACHE_PREFIX = 'лудик-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
