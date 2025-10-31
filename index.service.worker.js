6069924086815493bd46c40771e55891901c571e
vvsluv <uuu100500@mail.ru>
2025-10-31 10:34:07 +0300

    Фиксы утечек памяти

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761836189|3924414342';
+const CACHE_VERSION = '1761896032|63767607562';
 /** @type {string} */
 const CACHE_PREFIX = 'Gabler Life-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
