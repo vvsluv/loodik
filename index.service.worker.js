d52d5500036462a5cf7f5aad8d3e0edd17bdfbb0
vvsluv <uuu100500@mail.ru>
2025-11-01 13:39:30 +0300

    пацанов добавил

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761991786|14918088656';
+const CACHE_VERSION = '1761993563|16694597699';
 /** @type {string} */
 const CACHE_PREFIX = 'Gabler Life-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
