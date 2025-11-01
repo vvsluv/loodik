c952b891f2a172c884a69cec30fb97b3cae650c4
vvsluv <uuu100500@mail.ru>
2025-11-01 13:53:38 +0300

    пацанов добавил

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1761994000|17132196548';
+const CACHE_VERSION = '1761994411|17542822946';
 /** @type {string} */
-const CACHE_PREFIX = 'Gmabler Life-sw-cache-';
+const CACHE_PREFIX = 'Gambler Life-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
