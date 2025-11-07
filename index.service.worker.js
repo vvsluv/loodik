e9cd3e3055bdb12d4c48f127a7a7d175282a14cb
vvsluv <uuu100500@mail.ru>
2025-11-07 14:39:58 +0300

    Фиксы

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762510734|273525355';
+const CACHE_VERSION = '1762515578|5117640123';
 /** @type {string} */
 const CACHE_PREFIX = 'Gambler Life-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
